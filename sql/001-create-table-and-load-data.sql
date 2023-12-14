fd8a9409a86b6181ccd58c9031da9fd3cc8905ee
猪瀬 <dinose0806@icloud.com>
2023-12-14 09:21:17 +0900

    001-create-table-and-load-data.sqlのテーブル名、列名を映画から漫画仕様に変更

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -1,11 +1,9 @@
-DROP TABLE IF EXISTS movies;
-
-CREATE TABLE movies (
+CREATE TABLE anime (
   id int unsigned AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  director VARCHAR(100) NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  original_writer VARCHAR(100) NOT NULL,
   PRIMARY KEY(id)
 );
 
-INSERT INTO movies (name, director) VALUES ("ショーシャンクの空に", "フランク・ダラボン");
-INSERT INTO movies (name, director) VALUES ("この世界の片隅に", "片渕須直");
+INSERT INTO movies (title, original_writer) VALUES ("ジョジョの奇妙な冒険", "荒木飛呂彦");
+INSERT INTO movies (title, original_writer) VALUES ("銀魂", "空知英秋");
